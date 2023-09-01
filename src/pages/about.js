480f9d3c5b51ba4cf527e0fae57cb8c3db548dc4
soubj <sobujahmed124@gmail.com>
2023-09-01 23:22:53 +0600

    finished biography image and experience section of about me

---
+++ src/pages/about.js
@@ -0,0 +1,105 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/sobuj1.jpg";
+import Image from "next/image";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, motionValue, value]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Sobuj | About</title>
+
+        <meta name="description" content="about me" />
+      </Head>
+      <main className="w-full flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="About Me" className="text-center mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I'm Sobuj Ahmed, a web developer with a passion for creating
+                beautiful, functional, and user-centered digital experiences. I
+                am always looking for new and innovative ways to bring my
+                clients' visions to life.
+              </p>
+              <p className="font-medium my-4">
+                I believe that development is about more than just making things
+                look pretty – it's about solving problems and creating
+                intuitive, enjoyable experiences for users.
+              </p>
+              <p className="font-medium">
+                Whether I'm working on a website, or other digital product, I
+                bring my commitment to design excellence and user-centered
+                thinking to every project I work on. I look forward to the
+                opportunity to bring my skills and passion to your next project.
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                className="w-full h-auto rounded-2xl"
+                src={profilePic}
+                alt="profile"></Image>
+            </div>
+
+            {/* animating numbers */}
+            <div className="col-span-2 flex flex-col justify-between items-end">
+              <div className="flex flex-col items-end justify-center">
+                <span className="text-7xl font-bold inline-block">
+                  <AnimatedNumbers value={5}></AnimatedNumbers>+
+                </span>
+                <h2 className="font-medium text-xl text-dark/75 capitalize">
+                  Completed Projects
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="text-7xl font-bold inline-block">0+</span>
+                <h2 className="font-medium text-xl text-dark/75 capitalize">
+                  Years of Experience
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="text-7xl font-bold inline-block">0+</span>
+                <h2 className="font-medium text-xl text-dark/75 capitalize">
+                  Satisfied Customers
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default About;
