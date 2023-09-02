aac4f4fe418a74b2f7c2d7e4016e1c750b5b06c3
soubj <sobujahmed124@gmail.com>
2023-09-02 16:25:36 +0600

    image is showind during hover on article

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,16 +3,23 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
 import article2 from "../../public/images/articles/create loading screen in react js.jpg";
-import { motion } from "framer-motion";
+import article3 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import article4 from "../../public/images/articles/smooth scrolling in reactjs.png";
+import article5 from "../../public/images/articles/create modal component in react using react portals.png";
+import article6 from "../../public/images/articles/todo list app built using react redux and framer motion.png";
+import article7 from "../../public/images/articles/What is Redux with easy explanation.png";
+import article8 from "../../public/images/articles/What is higher order component in React.jpg";
+import { motion, useMotionValue } from "framer-motion";
 
 const FramerImage = motion(Image);
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="col-span-1 border border-solid w-full p-4 bg-light border-dark rounded-2xl">
+    <li className="col-span-1 border border-solid w-full p-4 bg-light border-dark rounded-2xl relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
       <Link
         href={link}
         target="_blank"
@@ -26,13 +33,65 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
       </Link>
 
       <Link href={link} alt={title} target="_blank">
-        <h2 className="capitalize my-2 text-2xl font-bold hover:underline underline-offset-2">
+        <h2 className="capitalize my-2 mt-4 text-2xl font-bold hover:underline underline-offset-2">
           {title}
         </h2>
       </Link>
 
       <p className="text-sm mb-2">{summary}</p>
       <span className="text-primary font-semibold">{time}</span>
+    </li>
+  );
+};
+
+const MovingImg = ({ title, img, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}>
+      <h3 className="text-lg capitalize font-semibold hover:underline">
+        {title}
+      </h3>
+      <FramerImage
+        ref={imgRef}
+        style={{ x: x, y: y }}
+        src={img}
+        alt={title}
+        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: { duration: 0.2 },
+        }}></FramerImage>
+    </Link>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
+  return (
+    <li
+      className="w-full relative p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark
+    border-r-4 border-b-4">
+      <MovingImg title={title} img={img} link={link}></MovingImg>
+      <span className="text-primary font-semibold pl-4">{date}</span>
     </li>
   );
 };
@@ -64,6 +123,53 @@ const articles = () => {
               time="15 min read"
               link="/"
               img={article2}></FeaturedArticle>
+          </ul>
+
+          <h2 className="text-4xl font-bold text-center my-16 mt-32 w-full">
+            All Articles
+          </h2>
+          <ul>
+            {/* article 1 */}
+            <Article
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+              date="August 23, 2023"
+              img={article3}
+              link="/"></Article>
+
+            {/* article 2 */}
+            <Article
+              title="Silky Smooth Scrolling In Reactjs: A Step-By-Step Guide For React Developers"
+              date="August 29, 2023"
+              img={article4}
+              link="/"></Article>
+
+            {/* article 3 */}
+            <Article
+              title="Creating An Efficient Modal Component In React Using Hooks And Portals"
+              date="August 13, 2023"
+              img={article5}
+              link="/"></Article>
+
+            {/* article 4 */}
+            <Article
+              title="Build A Fabulous Todo List App With React, Redux And Framer-Motion"
+              date="September 3, 2023"
+              img={article6}
+              link="/"></Article>
+
+            {/* article 5 */}
+            <Article
+              title="Redux Simplified: A Beginner's Guide For Web Developers"
+              date="September 1, 2023"
+              img={article7}
+              link="/"></Article>
+
+            {/* article 6 */}
+            <Article
+              title="What Is Higher Order Component (Hoc) In React?"
+              date="September 10, 2023"
+              img={article8}
+              link="/"></Article>
           </ul>
         </Layout>
       </main>
