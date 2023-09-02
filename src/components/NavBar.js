bfa123db68155b0078655471dc59ca916cf0d52a
soubj <sobujahmed124@gmail.com>
2023-09-02 23:31:43 +0600

    created mobile navbar which closes after clicking on a link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,6 +32,32 @@ const CustomLink = ({ href, title, className = "" }) => {
   );
 };
 
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  // this useRouter() hook is similar to useLocation hook
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      className={`${className} relative group`}
+      onClick={handleClick}>
+      {title}
+      <span
+        className={`inline-block bg-dark h-[2px] absolute left-0 -bottom-0.5
+        group-hover:w-full transition-[width] ease duration-300 dark:bg-light ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}>
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
 const NavBar = () => {
   const [mode, setMode] = useThemeSwitcher();
   const [isOpen, setIsOpen] = useState(false);
@@ -56,55 +82,57 @@ const NavBar = () => {
           }`}></span>
       </button>
 
-      <nav>
-        <CustomLink href="/" title="Home" className="mr-4"></CustomLink>
-        <CustomLink href="/about" title="About" className="mx-4"></CustomLink>
-        <CustomLink
-          href="/projects"
-          title="Projects"
-          className="mx-4"></CustomLink>
-        <CustomLink
-          href="/articles"
-          title="Articles"
-          className="ml-4"></CustomLink>
-      </nav>
+      {/* desktop menu */}
+      <div className="w-full flex items-center justify-between lg:hidden">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4"></CustomLink>
+          <CustomLink href="/about" title="About" className="mx-4"></CustomLink>
+          <CustomLink
+            href="/projects"
+            title="Projects"
+            className="mx-4"></CustomLink>
+          <CustomLink
+            href="/articles"
+            title="Articles"
+            className="ml-4"></CustomLink>
+        </nav>
 
-      {/* social logo */}
-      <nav className="flex justify-center items-center flex-wrap">
-        {/* twitter */}
-        <motion.a
-          href="https://twitter.com/Sobuj03794254"
-          target="_blank"
-          whileHover={{
-            y: -2,
-          }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mr-3">
-          <TwitterIcon></TwitterIcon>
-        </motion.a>
-        {/* github */}
-        <motion.a
-          href="https://github.com/Sobuj54"
-          target="_blank"
-          whileHover={{
-            y: -2,
-          }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3">
-          <GithubIcon></GithubIcon>
-        </motion.a>
-        {/* linkedIn */}
-        <motion.a
-          href="https://www.linkedin.com/in/sobuj-ahmed-8a782b177/"
-          target="_blank"
-          whileHover={{
-            y: -2,
-          }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3">
-          <LinkedInIcon></LinkedInIcon>
-        </motion.a>
-        {/* pinterest
+        {/* social logo */}
+        <nav className="flex justify-center items-center flex-wrap">
+          {/* twitter */}
+          <motion.a
+            href="https://twitter.com/Sobuj03794254"
+            target="_blank"
+            whileHover={{
+              y: -2,
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mr-3">
+            <TwitterIcon></TwitterIcon>
+          </motion.a>
+          {/* github */}
+          <motion.a
+            href="https://github.com/Sobuj54"
+            target="_blank"
+            whileHover={{
+              y: -2,
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3">
+            <GithubIcon></GithubIcon>
+          </motion.a>
+          {/* linkedIn */}
+          <motion.a
+            href="https://www.linkedin.com/in/sobuj-ahmed-8a782b177/"
+            target="_blank"
+            whileHover={{
+              y: -2,
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3">
+            <LinkedInIcon></LinkedInIcon>
+          </motion.a>
+          {/* pinterest
         <motion.a
           href="/"
           target="_blank"
@@ -115,8 +143,8 @@ const NavBar = () => {
           className="w-6 mx-3 bg-light rounded-full">
           <PinterestIcon></PinterestIcon>
         </motion.a> */}
-        {/* dribble */}
-        {/* <motion.a
+          {/* dribble */}
+          {/* <motion.a
           href="/"
           target="_blank"
           whileHover={{
@@ -127,18 +155,118 @@ const NavBar = () => {
           <DribbbleIcon></DribbbleIcon>
         </motion.a> */}
 
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={`ml-4 flex items-center justify-center p-1 rounded-full ${
-            mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
-          }`}>
-          {mode === "dark" ? (
-            <SunIcon className="fill-dark"></SunIcon>
-          ) : (
-            <MoonIcon className="fill-dark"></MoonIcon>
-          )}
-        </button>
-      </nav>
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-4 flex items-center justify-center p-1 rounded-full ${
+              mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+            }`}>
+            {mode === "dark" ? (
+              <SunIcon className="fill-dark"></SunIcon>
+            ) : (
+              <MoonIcon className="fill-dark"></MoonIcon>
+            )}
+          </button>
+        </nav>
+      </div>
+
+      {/* mobile menu */}
+      {isOpen ? (
+        <div className="min-w-[70vw] flex flex-col items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/90 rounded-lg backdrop-blur-md py-32">
+          <nav className="flex flex-col items-center justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Home"
+              className="mr-4"
+              toggle={() => setIsOpen(!isOpen)}></CustomMobileLink>
+            <CustomMobileLink
+              href="/about"
+              title="About"
+              className="mx-4"
+              toggle={() => setIsOpen(!isOpen)}></CustomMobileLink>
+            <CustomMobileLink
+              href="/projects"
+              title="Projects"
+              className="mx-4"
+              toggle={() => setIsOpen(!isOpen)}></CustomMobileLink>
+            <CustomMobileLink
+              href="/articles"
+              title="Articles"
+              className="ml-4"
+              toggle={() => setIsOpen(!isOpen)}></CustomMobileLink>
+          </nav>
+
+          {/* social logo */}
+          <nav className="flex justify-center items-center flex-wrap">
+            {/* twitter */}
+            <motion.a
+              href="https://twitter.com/Sobuj03794254"
+              target="_blank"
+              whileHover={{
+                y: -2,
+              }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mr-3">
+              <TwitterIcon></TwitterIcon>
+            </motion.a>
+            {/* github */}
+            <motion.a
+              href="https://github.com/Sobuj54"
+              target="_blank"
+              whileHover={{
+                y: -2,
+              }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3">
+              <GithubIcon></GithubIcon>
+            </motion.a>
+            {/* linkedIn */}
+            <motion.a
+              href="https://www.linkedin.com/in/sobuj-ahmed-8a782b177/"
+              target="_blank"
+              whileHover={{
+                y: -2,
+              }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3">
+              <LinkedInIcon></LinkedInIcon>
+            </motion.a>
+            {/* pinterest
+        <motion.a
+          href="/"
+          target="_blank"
+          whileHover={{
+            y: -2,
+          }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3 bg-light rounded-full">
+          <PinterestIcon></PinterestIcon>
+        </motion.a> */}
+            {/* dribble */}
+            {/* <motion.a
+          href="/"
+          target="_blank"
+          whileHover={{
+            y: -2,
+          }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3">
+          <DribbbleIcon></DribbbleIcon>
+        </motion.a> */}
+
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`ml-4 flex items-center justify-center p-1 rounded-full ${
+                mode === "light" ? "bg-dark text-light" : "bg-light text-dark"
+              }`}>
+              {mode === "dark" ? (
+                <SunIcon className="fill-dark"></SunIcon>
+              ) : (
+                <MoonIcon className="fill-dark"></MoonIcon>
+              )}
+            </button>
+          </nav>
+        </div>
+      ) : null}
 
       {/* middle logo */}
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
