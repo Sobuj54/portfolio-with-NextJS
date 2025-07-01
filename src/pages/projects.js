1eedd165e84437426ea8a1ab2146cafa04e70966
sobuj54 <sobujahmed124@gmail.com>
2025-07-02 01:00:33 +0600

    added tastebite front end react project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,6 +16,7 @@ import jerinsParlour from "../../public/images/projects/Jerins Parlour.png";
 import selhono from "../../public/images/projects/selhono.png";
 import summerCamp from "../../public/images/projects/summer-camp.png";
 import youtube from "../../public/images/projects/youtube backend.png";
+import tastebite from "../../public/images/projects/tastebite.png";
 import { motion } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
 
@@ -23,30 +24,33 @@ const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+    <article className="relative flex items-center justify-between w-full p-12 border border-solid shadow-2xl rounded-3xl border-dark bg-light rounded-br-2xl dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
       {/* this creates shadow like dark in sections */}
       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
 
       <Link
         href={link}
         target="_blank"
-        className="w-1/2 overflow-hidden rounded-lg lg:w-full">
+        className="w-1/2 overflow-hidden rounded-lg lg:w-full"
+      >
         <FramerImage
           src={img}
           alt={title}
           className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}></FramerImage>
+          transition={{ duration: 0.2 }}
+        ></FramerImage>
       </Link>
 
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
+      <div className="flex flex-col items-start justify-between w-1/2 pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-xl font-medium text-primary dark:text-primaryDark xs:text-base">
           {type}
         </span>
         <Link
           href={link}
           target="_blank"
-          className="hover:underline underline-offset-4">
+          className="hover:underline underline-offset-4"
+        >
           <h2 className="my-2 w-full font-bold text-3xl text-left dark:text-light sm:text-[20px]">
             {title}
           </h2>
@@ -55,19 +59,21 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           {summary}
         </p>
 
-        <div className="mt-2 flex items-center dark:text-light">
+        <div className="flex items-center mt-2 dark:text-light">
           <Link
             href={github}
             target="_blank"
             className="w-10"
             data-te-toggle="tooltip"
-            title="Visit Repo">
+            title="Visit Repo"
+          >
             <GithubIcon></GithubIcon>
           </Link>
           <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark hover:bg-light hover:text-dark hover:border-dark text-light py-2 px-6 font-semibold text-lg dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:border-light hover:dark:text-light border hover:dark:border sm:px-4 sm:text-base">
+            className="px-6 py-2 ml-4 text-lg font-semibold border rounded-lg bg-dark hover:bg-light hover:text-dark hover:border-dark text-light dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:border-light hover:dark:text-light hover:dark:border sm:px-4 sm:text-base"
+          >
             Visit Project
           </Link>
         </div>
@@ -78,13 +84,14 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 
 const Projects = ({ type, title, img, link, github, summary }) => {
   return (
-    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light dark:text-light xs:p-4">
+    <article className="relative flex flex-col items-center justify-center w-full p-6 border border-solid rounded-2xl border-dark bg-light dark:bg-dark dark:border-light dark:text-light xs:p-4">
       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
 
       <Link
         href={link}
         target="_blank"
-        className="w-full overflow-hidden rounded-lg">
+        className="w-full overflow-hidden rounded-lg"
+      >
         <FramerImage
           src={img}
           alt={title}
@@ -94,17 +101,19 @@ const Projects = ({ type, title, img, link, github, summary }) => {
           priority
           sizes="(max-width: 768px) 100vw,
         (max-width: 1200px) 50vw,
-        50vw"></FramerImage>
+        50vw"
+        ></FramerImage>
       </Link>
 
-      <div className="w-full flex flex-col items-start justify-between mt-4 ">
-        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
+      <div className="flex flex-col items-start justify-between w-full mt-4 ">
+        <span className="text-xl font-medium text-primary dark:text-primaryDark lg:text-lg md:text-base">
           {type}
         </span>
         <Link
           href={link}
           target="_blank"
-          className="hover:underline underline-offset-4">
+          className="hover:underline underline-offset-4"
+        >
           <h2 className="my-2 w-full font-bold text-3xl text-left lg:text-2xl sm:text-[20px]">
             {title}
           </h2>
@@ -113,11 +122,12 @@ const Projects = ({ type, title, img, link, github, summary }) => {
           </p>
         </Link>
 
-        <div className="mt-2 w-full flex items-center justify-between">
+        <div className="flex items-center justify-between w-full mt-2">
           <Link
             href={link}
             target="_blank"
-            className="font-semibold text-lg underline underline-offset-2 md:text-base">
+            className="text-lg font-semibold underline underline-offset-2 md:text-base"
+          >
             Visit
           </Link>
 
@@ -126,7 +136,8 @@ const Projects = ({ type, title, img, link, github, summary }) => {
             target="_blank"
             className="w-8 md:w-6"
             data-te-toggle="tooltip"
-            title="Visit Repo">
+            title="Visit Repo"
+          >
             <GithubIcon></GithubIcon>
           </Link>
         </div>
@@ -144,11 +155,12 @@ const projects = () => {
         <meta name="description" content="projects" />
       </Head>
       <TransitionEffect></TransitionEffect>
-      <main className="w-full mb-16 flex flex-col items-center justify-center ">
+      <main className="flex flex-col items-center justify-center w-full mb-16 ">
         <Layout className="pt-16">
           <AnimatedText
             text="My Projects"
-            className="text-center mb-16 lg:!text-5xl sm:mb-8 sm:!text-4xl"></AnimatedText>
+            className="text-center mb-16 lg:!text-5xl sm:mb-8 sm:!text-4xl"
+          ></AnimatedText>
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
@@ -157,17 +169,32 @@ const projects = () => {
                 link="https://selhono-house.netlify.app/"
                 type="Featured Project"
                 img={selhono}
-                github="https://github.com/Sobuj54/selhono"></FeaturedProject>
+                github="https://github.com/Sobuj54/selhono"
+              ></FeaturedProject>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
               <Projects
                 title="Youtube Backend"
                 summary="A youtube backend application which handles authentication, video upload, like, dislike, comment, view, subscription, playlist, channel. This application was created using NodeJs, Express, MongoDB,Mongoose, JWT, Bcrypt, Multer, Cloudinary"
-                link=""
+                link="#"
                 type="Featured Project"
                 img={youtube}
-                github="https://github.com/Sobuj54/youtube-backend-professional-project"></Projects>
+                github="https://github.com/Sobuj54/youtube-backend-professional-project"
+              ></Projects>
+            </div>
+
+            <div className="col-span-6 sm:col-span-12">
+              <Projects
+                title="TasteBite"
+                summary="A comprehensive restaurant management system built with React that showcases advanced frontend development skills. This responsive web application features intuitive menu browsing, realtime order tracking, and a dynamic loyalty program interface. The project demonstrates my
+                expertise in component architecture, state management, and creating engaging user experiences
+                that solve real business challenges"
+                link="https://tastebite-three.vercel.app/menu-browse-search"
+                type="Featured Project"
+                img={tastebite}
+                github="https://github.com/Sobuj54/TasteBite-Restaurant-App"
+              ></Projects>
             </div>
 
             <div className="col-span-12">
@@ -178,7 +205,8 @@ const projects = () => {
                 link="https://summer-camp-345fa.web.app/"
                 type="Featured Project"
                 img={summerCamp}
-                github="https://github.com/Sobuj54/module-82-summer-camp-client"></FeaturedProject>
+                github="https://github.com/Sobuj54/module-82-summer-camp-client"
+              ></FeaturedProject>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -189,7 +217,8 @@ const projects = () => {
                 link="https://toy-emporium-d0aca.web.app/"
                 type="Featured Project"
                 img={toyEmporium}
-                github="https://github.com/Sobuj54/module-73-toy-emporium-client-assignment-"></Projects>
+                github="https://github.com/Sobuj54/module-73-toy-emporium-client-assignment-"
+              ></Projects>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -200,7 +229,8 @@ const projects = () => {
                 link="https://doc-house-1030e.web.app/"
                 type="Featured Project"
                 img={docHouse}
-                github="https://github.com/Sobuj54/module-76-doc-house-client-practice"></Projects>
+                github="https://github.com/Sobuj54/module-76-doc-house-client-practice"
+              ></Projects>
             </div>
 
             <div className="col-span-12">
@@ -211,7 +241,8 @@ const projects = () => {
                 link="https://jerins-parlour-275cf.web.app/"
                 type="Featured Project"
                 img={jerinsParlour}
-                github="https://github.com/Sobuj54/module-81-jerins-parlour-client"></FeaturedProject>
+                github="https://github.com/Sobuj54/module-81-jerins-parlour-client"
+              ></FeaturedProject>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -222,7 +253,8 @@ const projects = () => {
                 link="https://chef-recipe-cuisine.web.app/"
                 type="Featured Project"
                 img={chefsChoice}
-                github="https://github.com/Sobuj54/module-65-assignment-chef-recipe"></Projects>
+                github="https://github.com/Sobuj54/module-65-assignment-chef-recipe"
+              ></Projects>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -233,7 +265,8 @@ const projects = () => {
                 link="https://cars-doctor-b3f71.web.app/"
                 type="Featured Project"
                 img={carsDoctor}
-                github="https://github.com/Sobuj54/module-69-car-doctor-client"></Projects>
+                github="https://github.com/Sobuj54/module-69-car-doctor-client"
+              ></Projects>
             </div>
 
             <div className="col-span-12">
@@ -244,7 +277,8 @@ const projects = () => {
                 link="https://bistro-boss-5e1ed.web.app/"
                 type="Featured Project"
                 img={bistroBoss}
-                github="https://github.com/Sobuj54/module-74-bistro-boss-client"></FeaturedProject>
+                github="https://github.com/Sobuj54/module-74-bistro-boss-client"
+              ></FeaturedProject>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -255,7 +289,8 @@ const projects = () => {
                 link="https://the-news-dragon-6ca0a.web.app/categories/0"
                 type="Featured Project"
                 img={newsDragon}
-                github="https://github.com/Sobuj54/module-62-the-news-dragon-responsive-react-site"></Projects>
+                github="https://github.com/Sobuj54/module-62-the-news-dragon-responsive-react-site"
+              ></Projects>
             </div>
 
             <div className="col-span-6 sm:col-span-12">
@@ -266,7 +301,8 @@ const projects = () => {
                 link="https://volunteer-network-d5cab.web.app/"
                 type="Featured Project"
                 img={volunteerNetwork}
-                github="https://github.com/Sobuj54/module-72-volunteer-network-client"></Projects>
+                github="https://github.com/Sobuj54/module-72-volunteer-network-client"
+              ></Projects>
             </div>
           </div>
         </Layout>
